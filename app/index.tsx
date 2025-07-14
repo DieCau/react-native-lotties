c030a619763fc67236eaa04b6d432811142ad8a5
Diego Caucota <diegocaucota@gmail.com>
2025-07-14 20:45:27 -0300

    add: app index

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,17 @@
+import LottieView from 'lottie-react-native'; // Ensure you have this package installed
+import { SafeAreaView } from "react-native";
+
+import car from '../assets/lotties/car.json';
 
 export default function index() {
-  return null;
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <LottieView        
+        source={car}
+        autoPlay
+        loop
+        style={{ width: 200, height: 200 }}
+      />
+    </SafeAreaView>
+  )
 }
