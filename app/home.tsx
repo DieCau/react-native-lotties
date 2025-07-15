e1d2decaaf6efe47f8a3460f818a3e13c0dc559a
Diego Caucota <diegocaucota@gmail.com>
2025-07-15 14:41:10 -0300

    add: app home lottie passport

--- app/home.tsx
+++ app/home.tsx
@@ -1,6 +1,28 @@
+import LottieView from "lottie-react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
+
+import passport from '@/assets/lotties/passport.json';
 
 export default function home() {
   return (
-    null
+    <SafeAreaView style={styles.safeArea}>
+      <LottieView
+        source={passport}
+        autoPlay
+        loop
+        style={styles.lottie}
+      /> 
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+  lottie: {
+    width: "100%",
+    height: 200,
+  },  
+})  
